b3dc869c10237cb4613fceaa134a808a1fa12d78
Heptazhou <zhou@0h7z.com>
2023-03-11 08:49:20 +0000

    Update

--- wpf/make.jl
+++ wpf/make.jl
@@ -17,10 +17,27 @@ const a = `-m0=lzma -md1024m -mfb273 -mmt -ms -mx9 -stl -xr!\*.pdb`
 const c = "Release"
 const f = "net7.0-windows"
 const n = "Clock"
+const z = "clock-utc-net7"
+
+function fmt(dir::String)
+	cd(dir) do
+		cp("$(@__DIR__)/notice.txt", "NOTICE.txt")
+		r = r"^\t*\K {2}"m
+		for f ∈ filter!(endswith(".json"), readdir())
+			s = read(f, String)
+			s = replace(s, "\r\n" => "\n")
+			while contains(s, r)
+				s = replace(s, r => "\t")
+			end
+			isempty(s) || endswith(s, "\n") || (s *= "\n")
+			write(f, s)
+		end
+	end
+end
 
 try
 	cd(@__DIR__)
-	rm("$n.7z", force = true)
+	rm("$z.7z", force = true)
 	rm("$n.sln", force = true)
 	rm("$n/obj/", force = true, recursive = true)
 	rm("$n/bin/", force = true, recursive = true)
@@ -28,12 +45,15 @@ try
 	run(`dotnet new sln -n $n`)
 	run(`dotnet sln add $(n)`)
 	run(`dotnet build -c $c`)
+	fmt("$n/bin/$c/$f/")
 	mv("$n/bin/$c/$f/", "$n/bin/$c/$n/")
-	run(`7z a $a $n.7z ./$n/bin/$c/$n/`)
+	run(`7z a $a $z.7z ./$n/bin/$c/$n/`)
 	rm("$n/bin/$c/$n", recursive = true)
 	rm("$n.sln")
 catch e
 	@info e
 end
-isempty(ARGS) && readline()
+isempty(ARGS) || exit()
+print("\n> ")
+readline()
 
